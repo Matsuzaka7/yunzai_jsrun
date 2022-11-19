c3d115cd2f3961d0eba5273ed6065c4a235be8a4
松坂砂糖 <83869273+Matsuzaka7@users.noreply.github.com>
2022-11-19 10:12:38 +0800

    Update js运行器.js

--- js运行器.js
+++ js运行器.js
@@ -2,7 +2,8 @@ import { segment } from "oicq";
 // import axios from 'axios'
 
 /*
-  该插件在使用时可能会有不可预料的风险，请谨慎使用。包括但不限于：云崽崩溃，内存占满
+  jsrun - v0.3
+  该插件在使用时可能会有不可预料的风险，请谨慎使用。包括但不限于：云崽崩溃，内存溢出
   该插件的作用：
       - 提供js运行的环境，可执行js代码
       - 如： 
@@ -21,16 +22,30 @@ import { segment } from "oicq";
     - by 砂糖
 */
 
+// 缓存信息
+let tempRes = ''
+let resCount = 0
+
+// 设置与上一次的相应间隔 200ms
+let tempTime = 0
+let resTime = 200
+
+// 重复的话术
+const oneAdd1 = '与上一次运行结果一致'
+
 export class example extends plugin {
   constructor() {
     super({
-      name: 'js运行工具-by砂糖',
+      name: 'js运行工具',
       event: 'message',
       priority: 500,
     })
   }
 
+  //执行方法
   async accept(e) {
+      if (Date.now() - tempTime < resTime) return 
+      tempTime = Date.now()
       try {
         if (e.message[0].type !== 'text') return
         if (!e.message[0].text.includes('##')) return
@@ -44,29 +59,26 @@ export class example extends plugin {
         }
 
         let res = await eval(content);
-        // if (res && res.then) return
+        if (JSON.stringify(res.data || res) == tempRes) throw new Error(oneAdd1)
+        const length = 199
         if (typeof res !== "object") {
           await e.reply(`${res}`.trim());
         } else {
-          if (res.data) {
-            const MaxLength = 300
-            if (JSON.stringify(res.data).length > MaxLength) {
-               await e.reply(`字符长度超出${MaxLength}，进行截取`);
-               await e.reply(JSON.stringify(res.data, null, 4).substring(0, MaxLength) + '\n...');
-            } else {
-              await e.reply(JSON.stringify(res.data, null, 4));
-            }
+          const dataType = res.data || res;
+          if (JSON.stringify(dataType).length > length) {
+               await e.reply(`字符长度超出${length}，进行截取`);
+               if (res.data) return 
+               await e.reply(JSON.stringify(dataType, null, 4).substring(0, length) + '\n...');
           } else {
-            await e.reply(JSON.stringify(res, null, 4));
+              await e.reply(JSON.stringify(dataType, null, 4));
           }
-          
         }
+        resCount = 0
+        tempRes = JSON.stringify(res.data || res)
       } catch(error) {
-        console.log(error.message)
-        await e.reply('错误：' + error.message);
+        if (error.message === oneAdd1) resCount++;
+        if (resCount <= 1) await e.reply('错误：' + error.message);
       }
-      
     return true;
   }
-
 }
